0af6f3afed97938df4b3eb80cf78ebf23ab6d8c5
David Sanders <davesque@gmail.com>
2016-09-29 14:01:19 -0600

    Add dynamic macro functionality

--- keyboards/atomic/keymaps/davesque/keymap.c
+++ keyboards/atomic/keymaps/davesque/keymap.c
@@ -12,6 +12,13 @@
 #define _LW 3
 #define _RS 4
 #define _FN 5
+#define _DYN 6
+
+enum atomic_keycodes {
+  DYNAMIC_MACRO_RANGE = SAFE_RANGE
+};
+
+#include "dynamic_macro.h"
 
 /* ROW 1 OPTIONS
  * .--------------------------------------------------------------------------------------------------------------------------------------.
@@ -93,11 +100,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QW] = { /* QWERTY */
-  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_PGUP  },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN  },
-  { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,  KC_VOLU  },
-  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_VOLD  },
-  { MO(_LW), MO(_FN), KC_LGUI, KC_LCTL, KC_LALT, KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, MO(_FN), MO(_RS), KC_LEFT, KC_DOWN, KC_RGHT  },
+  { KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_PGUP  },
+  { KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN  },
+  { KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,  KC_VOLU  },
+  { KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_VOLD  },
+  { MO(_DYN), MO(_FN), KC_LGUI, KC_LCTL, KC_LALT, KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, MO(_FN), MO(_RS), KC_LEFT, KC_DOWN, KC_RGHT  },
  },
 
 /* COLEMAK - MIT ENHANCED / GRID COMPATIBLE
@@ -115,11 +122,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 	
  [_CM] = { /* COLEMAK */
-  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_PGUP  },
-  { KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN  },
-  { KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_ENT,  KC_ENT,  KC_VOLU  },
-  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_VOLD  },
-  { MO(_LW), MO(_FN), KC_LGUI, KC_LCTL, KC_LALT, KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, MO(_FN), MO(_RS), KC_LEFT, KC_DOWN, KC_RGHT  },
+  { KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_PGUP  },
+  { KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN  },
+  { KC_LCTL,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_ENT,  KC_ENT,  KC_VOLU  },
+  { KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_VOLD  },
+  { MO(_DYN), MO(_FN), KC_LGUI, KC_LCTL, KC_LALT, KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, MO(_FN), MO(_RS), KC_LEFT, KC_DOWN, KC_RGHT  },
  },
 	
 /* DVORAK - MIT ENHANCED / GRID COMPATIBLE
@@ -137,11 +144,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_DV] = { /* DVORAK */
-  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_PGUP  },
-  { KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN  },
-  { KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH, KC_ENT,  KC_ENT,  KC_VOLU  },
-  { KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, KC_RSFT, KC_UP,   KC_VOLD  },
-  { MO(_LW), MO(_FN), KC_LGUI, KC_LCTL, KC_LALT, KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, MO(_FN), MO(_RS), KC_LEFT, KC_DOWN, KC_RGHT  },
+  { KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_PGUP  },
+  { KC_TAB,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN  },
+  { KC_LCTL,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH, KC_ENT,  KC_ENT,  KC_VOLU  },
+  { KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, KC_RSFT, KC_UP,   KC_VOLD  },
+  { MO(_DYN), MO(_FN), KC_LGUI, KC_LCTL, KC_LALT, KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, MO(_FN), MO(_RS), KC_LEFT, KC_DOWN, KC_RGHT  },
  },
 
 /* LOWERED
@@ -209,11 +216,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { _______, _______, DF(_QW), DF(_CM), DF(_DV), _______, _______, _______, _______, _______, _______, ___T___, ___T___, _______, KC_MUTE  },
   { _______, _______, _______, _______, _______, ___T___, ___T___, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT  },
  },
+
+/* DYNAMIC MACROS
+ * .--------------------------------------------------------------------------------------------------------------------------------------.
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
+ * |        | RECST1 | MACPL1 |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+- 2u ------------+--------|
+ * |        | RECST2 | MACPL2 |        |        |        |        |        |        |        |        |        | XXXXXX .        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+- 2u ---------------------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        | XXXXXX .        |        |        |
+ * |--------+--------+--------+--------+--------+- 2u ------------+--------+--------+--------+--------+-----------------+--------+--------|
+ * |        |        |        |        |        | XXXXXX .        |        |        |        |        |        |        |        |        |
+ * '--------------------------------------------------------------------------------------------------------------------------------------'
+ */
+
+ [_DYN] = { /* DYNAMIC MACROS */
+  { _______, _______,        _______,         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+  { _______, DYN_REC_START1, DYN_MACRO_PLAY1, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+  { _______, DYN_REC_START2, DYN_MACRO_PLAY2, _______, _______, _______, _______, _______, _______, _______, _______, _______, ___T___, ___T___, _______ },
+  { _______, _______,        _______,         _______, _______, _______, _______, _______, _______, _______, _______, ___T___, ___T___, _______, _______ },
+  { _______, _______,        _______,         _______, _______, ___T___, ___T___, _______, _______, _______, _______, _______, _______, _______, _______ },
+ },
 };
 
 const uint16_t PROGMEM fn_actions[] = {
 
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_record_dynamic_macro(keycode, record)) {
+    return false;
+  }
+
+  return true;
+}
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
