d010bf0ebed660b908b867ee9d02b95cc3980524
David Sanders <davesque@gmail.com>
2016-09-29 16:35:39 -0600

    Switch position of control and alt

--- keyboards/atomic/keymaps/davesque/keymap.c
+++ keyboards/atomic/keymaps/davesque/keymap.c
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN  },
   { KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,  KC_VOLU  },
   { KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_VOLD  },
-  { MO(_DYN), MO(_FN), KC_LGUI, KC_LCTL, KC_LALT, KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, MO(_FN), MO(_RS), KC_LEFT, KC_DOWN, KC_RGHT  },
+  { MO(_DYN), MO(_FN), KC_LGUI, KC_LALT, KC_LCTL, KC_SPC,  KC_SPC,  KC_RCTL, KC_RALT, KC_RGUI, MO(_FN), MO(_RS), KC_LEFT, KC_DOWN, KC_RGHT  },
  },
 
 /* COLEMAK - MIT ENHANCED / GRID COMPATIBLE
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN  },
   { KC_LCTL,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_ENT,  KC_ENT,  KC_VOLU  },
   { KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_VOLD  },
-  { MO(_DYN), MO(_FN), KC_LGUI, KC_LCTL, KC_LALT, KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, MO(_FN), MO(_RS), KC_LEFT, KC_DOWN, KC_RGHT  },
+  { MO(_DYN), MO(_FN), KC_LGUI, KC_LALT, KC_LCTL, KC_SPC,  KC_SPC,  KC_RCTL, KC_RALT, KC_RGUI, MO(_FN), MO(_RS), KC_LEFT, KC_DOWN, KC_RGHT  },
  },
 	
 /* DVORAK - MIT ENHANCED / GRID COMPATIBLE
@@ -148,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN  },
   { KC_LCTL,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH, KC_ENT,  KC_ENT,  KC_VOLU  },
   { KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, KC_RSFT, KC_UP,   KC_VOLD  },
-  { MO(_DYN), MO(_FN), KC_LGUI, KC_LCTL, KC_LALT, KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, MO(_FN), MO(_RS), KC_LEFT, KC_DOWN, KC_RGHT  },
+  { MO(_DYN), MO(_FN), KC_LGUI, KC_LALT, KC_LCTL, KC_SPC,  KC_SPC,  KC_RCTL, KC_RALT, KC_RGUI, MO(_FN), MO(_RS), KC_LEFT, KC_DOWN, KC_RGHT  },
  },
 
 /* LOWERED
